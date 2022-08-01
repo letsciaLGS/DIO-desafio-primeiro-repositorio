3d8baa8e839fe79fbfa7b02e811195a41f0ab5ab
letsciaLGS <leticiagsilva.lgs@gmail.com>
2022-08-01 15:17:04 -0300

    Inclusão certificados DIO

--- DIO/Bootcamps/Santander_Code_Girls_2022/Modulo_2 (Java)/Dominando IDES Java.md
+++
@@ -1,1 +0,0 @@
-# Dominando IDES Java
---
+++ DIO/Bootcamps/Santander_Code_Girls_2022/Modulo_2 (Java)/Eclipse/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 3;
+		int b = 2;
+		// TODO Auto-generated method stub
+System.out.println("Hello World! " + (a+b));*/
+	}
+
+}
